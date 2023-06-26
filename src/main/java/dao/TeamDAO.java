9ed1fe45af2335aca1c30b656519c7a539094016
WooSeok77 <wi8tkf@naver.com>
2023-06-26 16:58:41 +0900

    feat : StadiumDAO, TeamDAO

---
+++ src/main/java/dao/TeamDAO.java
@@ -0,0 +1,65 @@
+package dao;
+
+import model.Stadium;
+import model.Team;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TeamDAO {
+
+    private Connection connection;
+
+    public TeamDAO(Connection connection) {
+        this.connection = connection;
+    }
+
+    //구현해야될 기능 : 팀등록, 전체팀 목록
+
+    //팀등록
+    //@Param
+    public void registerTeam(int stadiumId, String name) throws SQLException {
+        String query = "INSERT INTO team (stadium_id, name) VALUES (?,?)";
+
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setInt(1, stadiumId);
+            statement.setString(2,name);
+            statement.executeUpdate();
+        }
+    }
+
+    //전체팀목록조회
+    //List<Team>
+    //try catch SQLexception
+    public List<Team> findAll() {
+        List<Team> teamList = new ArrayList<>();
+        String query = "SELECT * FROM team";
+        try {
+            PreparedStatement statement = connection.prepareStatement(query);
+            ResultSet resultSet = statement.executeQuery();
+
+            while(resultSet.next()){
+                Team team = new Team(
+                        resultSet.getInt("id"),
+                        resultSet.getInt("stadiumId"),
+                        resultSet.getString("name"),
+                        resultSet.getTimestamp("createdAt")
+                );
+                teamList.add(team);
+            }
+            return teamList;
+
+        }catch(SQLException e) {
+            e.printStackTrace();
+        }
+        return teamList;
+    }
+
+
+
+
+}
