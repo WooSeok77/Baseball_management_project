2ee476f53f2ee10a0dc218cc912a2b2ea7067edc
yunhyeonjin <h00j@naver.com>
2023-06-26 16:45:35 +0900

    Revert "Revert "chore : initial commit""
    This reverts commit 0f4ae90ac42a922d99f0efff61fea66dd6749e44.

---
+++ src/main/java/com/example/baseball_management_project/BaseballManagementProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.baseball_management_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseballManagementProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseballManagementProjectApplication.class, args);
+    }
+
+}
